6fc5a0ec2eadc3dfd311695a369650cca2fea69a
Johanna Eriksson <je224gs@student.lnu.se>
2025-07-07 19:19:52 +0200

    Add more keys to secrets template.

--- secrets_template.py
+++ secrets_template.py
@@ -1,5 +1,12 @@
 # secrets_template.py
+# Change this file to secrets.py and fill in your credentials.
 secrets = {
     'ssid': 'YOUR_WIFI_SSID',
-    'password': 'YOUR_WIFI_PASSWORD'
+    'password': 'YOUR_WIFI_PASSWORD',
+    'AIO_SERVER': 'YOUR_AIO_SERVER',
+    'AIO_PORT': 'YOUR_AIO_PORT',
+    'AIO_USER': 'YOUR_AIO_USERNAME',
+    'AIO_KEY': 'YOUR_AIO_KEY',
+    'AIO_FEED_TEMPHUM': 'YOUR_AIO_FEED_TEMPHUM',
+    'AIO_FEED_LIGHT': 'YOUR_AIO_FEED_LIGHT',
 }
